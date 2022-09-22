997eccb820734db0f0adb2f279451e41ba8a3279
joao.sipauba <joao.sipauba@ceuma.br>
2022-09-22 12:03:29 -0300

    test: criando unit test para a entidade order

---
+++ src/entity/order.spec.ts
@@ -0,0 +1,38 @@
+import Order from "./order";
+import OrderItem from "./order_item";
+
+describe("Order unit tests", () => {
+
+    it("should throw error when id is empty", () => {
+        expect(() => {
+            let order = new Order("", "123", []);
+        }).toThrowError("Order ID is required");
+    })
+
+    it("should throw error when customerID is empty", () => {
+        expect(() => {
+            let order = new Order("123", "", []);
+        }).toThrowError("Customer ID is required");
+    })
+
+    it("should throw error when items is empty", () => {
+        expect(() => {
+            let order = new Order("123", "123", []);
+        }).toThrowError("Items are required");
+    })
+
+    it("should calculate total", () => {
+
+        const item = new OrderItem("i1", "item 1", 100);
+        const item2 = new OrderItem("i2", "item 2", 200);
+        const order = new Order("o1", "c1", [item]);
+
+        let total = order.total();
+
+        expect(total).toBe(100);
+
+        const order2 = new Order("o2", "c1", [item, item2]);
+        total = order2.total();
+        expect(total).toBe(300);
+    })
+})
